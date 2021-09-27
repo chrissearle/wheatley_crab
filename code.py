0ff0d2c9f1a2e6b93fd0a7dffbfe989689ac397d
Chris Searle <chris@chrissearle.org>
2021-09-27 17:09:35 +0200

    Doesn't need adafruit_imageload after all

--- code.py
+++ code.py
@@ -11,25 +11,24 @@
 #  circup install gc9a01
 #
 
-import os
 import time
 import board
 import math
 import busio
 import terminalio
 import displayio
-import adafruit_imageload
 import gc9a01
 
 # A list of all the BMP images you want displayed, in order
 #
 # prepare image with ImageMagick like:
 # convert input.jpg -resize 240x240 -type palette BMP3:output.bmp
-img_filenames = ("/imgs/avatar.bmp",
-                 "/imgs/Fact.bmp",
-                 "/imgs/Rick.bmp",
-                 "/imgs/Space.bmp",
-                 "/imgs/Wheatley.bmp")
+img_filenames = (
+    "/imgs/Fact.bmp",
+    "/imgs/Rick.bmp",
+    "/imgs/Space.bmp",
+    "/imgs/Wheatley.bmp",
+)
 
 # time in seconds between images
 img_time = 10
@@ -38,8 +37,10 @@ img_time = 10
 displayio.release_displays()
 
 # attempt to auto-detect board type
+import os
+
 board_type = os.uname().machine
-if 'QT Py M0 Haxpress' in board_type or 'QT Py RP2040' in board_type:
+if "QT Py M0 Haxpress" in board_type or "QT Py RP2040" in board_type:
     tft_clk = board.SCK
     tft_mosi = board.MOSI
     tft_rst = board.TX
@@ -47,7 +48,7 @@ if 'QT Py M0 Haxpress' in board_type or 'QT Py RP2040' in board_type:
     tft_cs = board.A3
     tft_bl = board.A2
     spi = busio.SPI(clock=tft_clk, MOSI=tft_mosi)
-elif 'ItsyBitsy M4' in board_type:
+elif "ItsyBitsy M4" in board_type:
     tft_clk = board.SCK
     tft_mosi = board.MOSI
     tft_rst = board.MISO
@@ -55,7 +56,7 @@ elif 'ItsyBitsy M4' in board_type:
     tft_cs = board.A5
     tft_bl = board.A3  # optional
     spi = busio.SPI(clock=tft_clk, MOSI=tft_mosi)
-elif 'Pico' in board_type:
+elif "Pico" in board_type:
     # Raspberry Pi Pico pinout, one possibility, at "southwest" of board
     tft_clk = board.GP10  # must be a SPI CLK
     tft_mosi = board.GP11  # must be a SPI TX
@@ -68,10 +69,8 @@ else:
     print("ERROR: Unknown board!")
 
 # Make the displayio SPI bus and the GC9A01 display
-display_bus = displayio.FourWire(
-    spi, command=tft_dc, chip_select=tft_cs, reset=tft_rst)
-display = gc9a01.GC9A01(display_bus, width=240,
-                        height=240, backlight_pin=tft_bl)
+display_bus = displayio.FourWire(spi, command=tft_dc, chip_select=tft_cs, reset=tft_rst)
+display = gc9a01.GC9A01(display_bus, width=240, height=240, backlight_pin=tft_bl)
 
 # Make the main display context
 main = displayio.Group()
@@ -79,13 +78,13 @@ display.show(main)
 
 i = 0
 while True:
-    print(time.monotonic(), "hello")
+    print(time.monotonic(), "Loop")
     img_filename = img_filenames[i]
+    print(time.monotonic(), img_filename)
     img_bitmap = displayio.OnDiskBitmap(open(img_filename, "rb"))
     img_palette = displayio.ColorConverter()
-    #img_bitmap, img_palette = adafruit_imageload.load(img_filename)
     img_tilegrid = displayio.TileGrid(img_bitmap, pixel_shader=img_palette)
     main.append(img_tilegrid)
     time.sleep(img_time)
     main.pop()  # remove image
-    i = (i+1) % len(img_filenames)  # go to next file
+    i = (i + 1) % len(img_filenames)  # go to next file
